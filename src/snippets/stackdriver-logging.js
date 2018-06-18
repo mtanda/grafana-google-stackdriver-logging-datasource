26cdf139ec7eec9c82769455d4bb5739b8c5477a
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2018-06-18 18:21:12 +0900

    wip

--- src/snippets/stackdriver-logging.js
+++ src/snippets/stackdriver-logging.js
@@ -1,10 +1,10 @@
 // jshint ignore: start
 // jscs: disable
-ace.define("ace/snippets/stackdriver",["require","exports","module"], function(require, exports, module) {
+ace.define("ace/snippets/stackdriver-logging",["require","exports","module"], function(require, exports, module) {
 "use strict";
 
 exports.snippets = [
 ];
 
-exports.scope = "stackdriver";
+exports.scope = "stackdriver-logging";
 });
